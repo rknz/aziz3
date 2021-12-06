41d944b4812318dfc2d90e7beacd093502419808
rknz <77498227+rknz@users.noreply.github.com>
2021-12-06 22:54:30 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+// A $( document ).ready() block.
+$(document).ready(function () {
+    'use strict'
+    // =================================================================== Metires_ Bg On Hovar
+    $('.Metires_Item ul li').hover(function(){
+        $('#Metires_Bg').removeClass().addClass($(this).attr('rel')).addClass('mshow');
+        $(this).addClass('active').siblings().removeClass('active');
+    });
+    $('.Metires_Item ul li').mouseleave(function(){
+        $('#Metires_Bg').removeClass($(this).attr('rel'));
+    });
+    // =================================================================== Metires_ Bg On Hovar
+    AOS.init();
+
+});
